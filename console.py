99476f9a5de8844984ab0051a5ee95b3b8710d53
Emmanuel-NS <e.nsabagasa@alustudent.com>
2025-05-27 16:25:53 -0400

    updating console file

--- console.py
+++ console.py
@@ -1,12 +1,12 @@
 import cmd
 class HBNBCammand(cmd.Cmd):
+    '''console interpreter'''
     prompt="(hbnb) "
     def do_quit(self,arg):
         '''quiting the console'''
         return True
     def do_EOF(self,arg):
         '''End of the file'''
-        print()
         return True
     def emptyline(self):
         pass
