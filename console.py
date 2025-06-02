f1703247193983a1c9fb648c52b50c13f02d7892
Emmanuel-NS <e.nsabagasa@alustudent.com>
2025-06-02 17:42:45 -0400

    adding some file

--- console.py
+++ console.py
@@ -1,4 +1,7 @@
 import cmd
+import shlex
+from models.__init__ import storage
+from models.base_model import BaseModel
 class HBNBCammand(cmd.Cmd):
     '''console interpreter'''
     prompt="(hbnb) "
@@ -10,7 +13,63 @@ class HBNBCammand(cmd.Cmd):
         return True
     def emptyline(self):
         pass
+    def do_create(self,arg):
+        arg=shlex.split(arg)
+        classes=['BaseModel']
+        if not arg:
+            print("class name is missing")
+        elif arg[0] not in classes:
+            print("classname doesn't exist")
+        else:
+            inst=BaseModel()
+            inst.save()
+            print(inst.id)
 
+    def do_show(self,arg):
+        arg=shlex.split(arg)
+        classes=['BaseModel']
+        if arg:
+            if arg[0] not in classes:
+                print("class name doesn't exit")
+            else:
+                if len(arg)<2:
+                    print("instance id missing")
+                else:
+                    obj_key=f"{arg[0]}.{arg[1]}"
+                    objs=storage.all()
+                    result=False
+                    for key,obj in objs.items():
+                        if key== obj_key:
+                            print(obj)
+                            result=True
+                    if not result:
+                        print('no instance found')
+        else:
+            print("class name is missing")
+        
+    def do_destroy(self,arg):
+        arg=shlex.split(arg)
+        classes=['BaseModel']
+        if arg:
+            if arg[0] not in classes:
+                print("class name doesn't exit")
+            else:
+                if len(arg)<2:
+                    print("instance id missing")
+                else:
+                    obj_key=f"{arg[0]}.{arg[1]}"
+                    objs=storage.all()
+                    result=False
+                    for key,obj in objs.items():
+                        if key== obj_key:
+                            del storage.all()[key]
+                            storage.save()
+                            result=True
+                            break
+                    if not result:
+                        print('no instance found')
+        else:
+            print("class name is missing")
 if __name__=="__main__":
     obj=HBNBCammand()
     obj.cmdloop()
