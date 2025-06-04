a4aac01411f0cc679780034bfa45b3d436dce515
Emmanuel-NS <e.nsabagasa@alustudent.com>
2025-06-04 11:47:05 -0400

    creating file storage engine

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,11 @@
 import uuid
 from datetime import datetime
 
-# from models import storage
+from models import storage
 
 
 class BaseModel:
-    def __init__(self,*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
                 if key=='updated_at' or key=='created_at':
@@ -17,11 +17,11 @@ class BaseModel:
             self.id= str(uuid.uuid4())
             self.created_at= datetime.now()
             self.updated_at= datetime.now()
-            # storage.new(self)
+            storage.new(self)
 
     def save(self):
         self.updated_at=datetime.now()
-        # storage.save()
+        storage.save()
     
     def to_dict(self):
         d=self.__dict__.copy()
