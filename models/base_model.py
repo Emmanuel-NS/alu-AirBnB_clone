2a33a0281d26813f5ed79914b629374d89df3a77
Emmanuel-NS <e.nsabagasa@alustudent.com>
2025-06-04 11:02:16 -0400

    debugging

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-# from models import storage
+from models import storage
 
 
 class BaseModel:
@@ -17,11 +17,11 @@ class BaseModel:
             self.id= str(uuid.uuid4())
             self.created_at= datetime.now()
             self.updated_at= datetime.now()
-            # storage.new(self)
+            storage.new(self)
 
     def save(self):
         self.updated_at=datetime.now()
-        # storage.save()
+        storage.save()
     
     def to_dict(self):
         d=self.__dict__.copy()
