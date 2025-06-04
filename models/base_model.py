067e5c571d60ff68d7b990fcab2d179060d1a622
Emmanuel-NS <e.nsabagasa@alustudent.com>
2025-06-04 11:00:28 -0400

    adding debugging

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from models import storage
+# from models import storage
 
 
 class BaseModel:
@@ -17,23 +17,19 @@ class BaseModel:
             self.id= str(uuid.uuid4())
             self.created_at= datetime.now()
             self.updated_at= datetime.now()
-            storage.new(self)
+            # storage.new(self)
 
     def save(self):
         self.updated_at=datetime.now()
-        storage.save()
+        # storage.save()
     
     def to_dict(self):
         d=self.__dict__.copy()
         d['__class__']=self.__class__.__name__
-        d['created_at']=self.created_at.isoformat()
-        d['updated_at']=self.updated_at.isoformat()
+        d['created_at']=datetime.isoformat(self.created_at)
+        d['updated_at']=datetime.isoformat(self.created_at)
         return d
 
     def __str__(self):
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
-if __name__=="__main__":
-    obj=BaseModel()
-    obj.save()
-    print(obj)
