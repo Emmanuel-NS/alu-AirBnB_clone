89388d6f8efb8313f2e6c3fe1f1a31bf0bfb58d6
Emmanuel-NS <e.nsabagasa@alustudent.com>
2025-06-04 12:36:59 -0400

    debugging

--- console.py
+++ console.py
@@ -1,3 +1,9 @@
+"""console.py - Command interpreter for the AirBnB clone project.
+
+This module defines the HBNBCammand class, which provides a command-line
+interface to create, show, and destroy BaseModel instances.
+"""
+
 import cmd
 import shlex
 
@@ -6,73 +12,85 @@ from models.base_model import BaseModel
 
 
 class HBNBCammand(cmd.Cmd):
-    '''console interpreter'''
-    prompt="(hbnb) "
-    def do_quit(self,arg):
-        '''quiting the console'''
+    """Console interpreter for AirBnB clone.
+
+    Provides commands to create, show, and destroy BaseModel instances.
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit the console."""
         return True
-    def do_EOF(self,arg):
-        '''End of the file'''
+
+    def do_EOF(self, arg):
+        """Handle EOF (Ctrl+D) to exit the console."""
         return True
+
     def emptyline(self):
+        """Do nothing on empty input line."""
         pass
-    def do_create(self,arg):
-        arg=shlex.split(arg)
-        classes=['BaseModel']
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel and print its id."""
+        arg = shlex.split(arg)
+        classes = ['BaseModel']
         if not arg:
             print("class name is missing")
         elif arg[0] not in classes:
             print("classname doesn't exist")
         else:
-            inst=BaseModel()
+            inst = BaseModel()
             inst.save()
             print(inst.id)
 
-    def do_show(self,arg):
-        arg=shlex.split(arg)
-        classes=['BaseModel']
+    def do_show(self, arg):
+        """Show the string representation of an instance."""
+        arg = shlex.split(arg)
+        classes = ['BaseModel']
         if arg:
             if arg[0] not in classes:
                 print("class name doesn't exit")
             else:
-                if len(arg)<2:
+                if len(arg) < 2:
                     print("instance id missing")
                 else:
-                    obj_key=f"{arg[0]}.{arg[1]}"
-                    objs=storage.all()
-                    result=False
-                    for key,obj in objs.items():
-                        if key== obj_key:
+                    obj_key = f"{arg[0]}.{arg[1]}"
+                    objs = storage.all()
+                    result = False
+                    for key, obj in objs.items():
+                        if key == obj_key:
                             print(obj)
-                            result=True
+                            result = True
                     if not result:
                         print('no instance found')
         else:
             print("class name is missing")
-        
-    def do_destroy(self,arg):
-        arg=shlex.split(arg)
-        classes=['BaseModel']
+
+    def do_destroy(self, arg):
+        """Delete an instance based on the class name and id."""
+        arg = shlex.split(arg)
+        classes = ['BaseModel']
         if arg:
             if arg[0] not in classes:
                 print("class name doesn't exit")
             else:
-                if len(arg)<2:
+                if len(arg) < 2:
                     print("instance id missing")
                 else:
-                    obj_key=f"{arg[0]}.{arg[1]}"
-                    objs=storage.all()
-                    result=False
-                    for key,obj in objs.items():
-                        if key== obj_key:
+                    obj_key = f"{arg[0]}.{arg[1]}"
+                    objs = storage.all()
+                    result = False
+                    for key, obj in objs.items():
+                        if key == obj_key:
                             del storage.all()[key]
                             storage.save()
-                            result=True
+                            result = True
                             break
                     if not result:
                         print('no instance found')
         else:
             print("class name is missing")
-if __name__=="__main__":
-    obj=HBNBCammand()
+
+if __name__ == "__main__":
+    obj = HBNBCammand()
     obj.cmdloop()
