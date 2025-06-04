321cf38b840e63c2af3f10ecebd5b534426c8de7
Emmanuel-NS <e.nsabagasa@alustudent.com>
2025-06-04 11:29:12 -0400

    debugging

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,33 @@
 import uuid
 from datetime import datetime
 
-from models import storage
+# from models import storage
 
 
 class BaseModel:
-    def __init__(self,**kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key=='updated_at' or key=='created_at':
-                    value=datetime.fromisoformat(value)
-                if key=='__class__':
-                    continue
-                setattr(self,key,value)
-        else:
+    def __init__(self):
+        # if kwargs:
+        #     for key, value in kwargs.items():
+        #         if key=='updated_at' or key=='created_at':
+        #             value=datetime.fromisoformat(value)
+        #         if key=='__class__':
+        #             continue
+        #         setattr(self,key,value)
+        # else:
             self.id= str(uuid.uuid4())
             self.created_at= datetime.now()
             self.updated_at= datetime.now()
-            storage.new(self)
+            # storage.new(self)
 
     def save(self):
         self.updated_at=datetime.now()
-        storage.save()
+        # storage.save()
     
     def to_dict(self):
         d=self.__dict__.copy()
         d['__class__']=self.__class__.__name__
         d['created_at']=datetime.isoformat(self.created_at)
-        d['updated_at']=datetime.isoformat(self.created_at)
+        d['updated_at']=datetime.isoformat(self.updated_at)
         return d
 
     def __str__(self):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,32 @@
-import json
+# import json
 
 
-class FileStorage:
-    __file_path="file.json"
-    __objects={}
-    def all(self):
-        return FileStorage.__objects
+# class FileStorage:
+#     __file_path="file.json"
+#     __objects={}
+#     def all(self):
+#         return FileStorage.__objects
 
-    def new(self,obj):
-        key=f'{obj.__class__.__name__}.{obj.id}'
-        FileStorage.__objects[key]= obj
+#     def new(self,obj):
+#         key=f'{obj.__class__.__name__}.{obj.id}'
+#         FileStorage.__objects[key]= obj
     
-    def save(self):
-        dict={}
-        for key,obj in FileStorage.__objects.items():
-            dict[key]=obj.to_dict()
+#     def save(self):
+#         dict={}
+#         for key,obj in FileStorage.__objects.items():
+#             dict[key]=obj.to_dict()
         
-        with open(FileStorage.__file_path,'w') as file:
-            json.dump(dict,file)
+#         with open(FileStorage.__file_path,'w') as file:
+#             json.dump(dict,file)
             
-    def reload(self):
-        try:
-            from models.base_model import BaseModel
-            with open(FileStorage.__file_path,'r') as file:
-                data=json.load(file)
-            for key,value in data.items():
-                FileStorage.__objects[key]=BaseModel(**value)
+#     def reload(self):
+#         try:
+#             from models.base_model import BaseModel
+#             with open(FileStorage.__file_path,'r') as file:
+#                 data=json.load(file)
+#             for key,value in data.items():
+#                 FileStorage.__objects[key]=BaseModel(**value)
 
-        except FileNotFoundError:
-            pass
+#         except FileNotFoundError:
+#             pass
 
