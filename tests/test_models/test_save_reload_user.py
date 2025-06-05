ecea1aa02204fad99093961702a1606c7ec690b7
Emmanuel-NS <e.nsabagasa@alustudent.com>
2025-06-05 08:03:04 -0400

    updating testing

--- tests/test_models/test_save_reload_user.py
+++ tests/test_models/test_save_reload_user.py
@@ -2,6 +2,7 @@
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+import unittest
 
 all_objs = storage.all()
 print("-- Reloaded objects --")
@@ -25,3 +26,27 @@ my_user2.email = "airbnb2@mail.com"
 my_user2.password = "root"
 my_user2.save()
 print(my_user2)
+
+class TestUserAttributes(unittest.TestCase):
+    def test_email(self):
+        user = User()
+        self.assertTrue(hasattr(user, "email"))
+        self.assertEqual(user.email, "")
+
+    def test_password(self):
+        user = User()
+        self.assertTrue(hasattr(user, "password"))
+        self.assertEqual(user.password, "")
+
+    def test_first_name(self):
+        user = User()
+        self.assertTrue(hasattr(user, "first_name"))
+        self.assertEqual(user.first_name, "")
+
+    def test_last_name(self):
+        user = User()
+        self.assertTrue(hasattr(user, "last_name"))
+        self.assertEqual(user.last_name, "")
+
+if __name__ == "__main__":
+    unittest.main()
